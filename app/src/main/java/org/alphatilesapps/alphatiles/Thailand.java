f286f455b48c58d19899fc9eed82af2e7485be5f
craiglynne <78953023+craiglynne@users.noreply.github.com>
2021-02-18 19:24:00 +0100

    Update Thailand.java

--- app/src/main/java/org/alphatilesapps/alphatiles/Thailand.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/Thailand.java
@@ -1,10 +1,12 @@
 package org.alphatilesapps.alphatiles;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import java.util.logging.Logger;
 
-public class Thailand extends GameActivity {
+public class Thailand extends Activity //GameActivity
+{
 
     private static final String[] COLORS = {"#9C27B0", "#2196F3", "#F44336","#4CAF50","#E91E63"};
 
