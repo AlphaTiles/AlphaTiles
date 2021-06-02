14e81d47be1be9ae96ab4e4e7160269b27d7dfb7
laineyhm <56163492+laineyhm@users.noreply.github.com>
2021-06-01 21:13:21 -0400

    Update GameActivity.java
    Making GameActivity abstract. Bringing in imports, abstract variables, and common methods of the game classes.

--- app/src/main/java/org/alphatilesapps/alphatiles/GameActivity.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/GameActivity.java
@@ -2,15 +2,18 @@ package org.alphatilesapps.alphatiles;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;	
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameActivity extends AppCompatActivity {
+public abstract class GameActivity extends AppCompatActivity {
 
 	// KP, Oct 2020
 
@@ -20,6 +23,8 @@ public class GameActivity extends AppCompatActivity {
 	int challengeLevel = -1;
 	int playerNumber = -1;
 	int gameNumber = 0;
+	int visibleTiles;	
+	String className;
 
 	ArrayList<String> parsedWordArrayFinal;
 
@@ -34,6 +39,9 @@ public class GameActivity extends AppCompatActivity {
 			R.id.tracker11, R.id.tracker12
 
 	};
+	
+	protected abstract int[] getTileButtons();	
+	protected abstract int[] getWordImages();
 
 	@Override
 	protected void onCreate(Bundle state) {
@@ -43,6 +51,8 @@ public class GameActivity extends AppCompatActivity {
 		playerNumber = getIntent().getIntExtra("playerNumber", -1);
 		challengeLevel = getIntent().getIntExtra("challengeLevel", -1);
 		gameNumber = getIntent().getIntExtra("gameNumber", 0);
+		
+		className = getClass().getName();
 
 		super.onCreate(state);
 
@@ -107,6 +117,208 @@ public class GameActivity extends AppCompatActivity {
 
 		return count;
 
+	}
+	
+	protected void setAllTilesUnclickable()	
+	{	
+		for (int t = 0; t < visibleTiles; t++)	
+		{	
+			TextView gameTile = findViewById(getTileButtons()[t]);	
+			gameTile.setClickable(false);	
+		}	
+	}	
+	protected void setAllTilesClickable()	
+	{	
+		for (int t = 0; t < visibleTiles; t++)	
+		{	
+			TextView gameTile = findViewById(getTileButtons()[t]);	
+			gameTile.setClickable(true);	
+		}	
+	}	
+	protected void setOptionsRowUnclickable()	
+	{	
+		ImageView repeatImage = findViewById(R.id.repeatImage);	
+		ImageView wordImage = findViewById(R.id.wordImage);	
+		repeatImage.setBackgroundResource(0);	
+		repeatImage.setImageResource(R.drawable.zz_forward_inactive);	
+		repeatImage.setClickable(false);	
+		if (wordImage != null)	
+			wordImage.setClickable(false);	
+		if (getWordImages() != null)	
+			for (int i = 0; i < 4; i++)	
+			{	
+				wordImage = findViewById(getWordImages()[i]);	
+				wordImage.setClickable(false);	
+			}	
+	}	
+	protected void setOptionsRowClickable()	
+	{	
+		ImageView repeatImage = findViewById(R.id.repeatImage);	
+		ImageView wordImage = findViewById(R.id.wordImage);	
+		ImageView gamesHomeImage = findViewById(R.id.gamesHomeImage);	
+		repeatImage.setBackgroundResource(0);	
+		repeatImage.setImageResource(R.drawable.zz_forward);	
+		repeatImage.setClickable(true);	
+		gamesHomeImage.setClickable(true);	
+		if (wordImage != null)	
+			wordImage.setClickable(true);	
+		if (getWordImages() != null)	
+			for (int i = 0; i < 4; i++)	
+			{	
+				wordImage = findViewById(getWordImages()[i]);	
+				wordImage.setClickable(true);	
+			}	
+	}	
+	public void clickPicHearAudio(View view)	
+	{	
+		playActiveWordClip(false);	
+	}	
+/*	
+	protected void playActiveWordClip()	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		int resID = getResources().getIdentifier(wordInLWC, "raw", getPackageName());	
+		final MediaPlayer mp1 = MediaPlayer.create(this, resID);	
+		mediaPlayerIsPlaying = true;	
+		//mp1.start();	
+		mp1.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp1)	
+			{	
+				mpCompletion(mp1);	
+			}	
+		});	
+		mp1.start();	
+	}	
+*/	
+	protected void playActiveWordClip(final boolean playFinalSound)	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		int resID = getResources().getIdentifier(wordInLWC, "raw", getPackageName());	
+		final MediaPlayer mp1 = MediaPlayer.create(this, resID);	
+		mediaPlayerIsPlaying = true;	
+		//mp1.start();	
+		mp1.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp1)	
+			{	
+				mpCompletion(mp1, playFinalSound);	
+			}	
+		});	
+		mp1.start();	
+	}	
+/*	
+	protected void playCorrectSoundThenActiveWordClip()	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		MediaPlayer mp2 = MediaPlayer.create(this, R.raw.zz_correct);	
+		mediaPlayerIsPlaying = true;	
+		mp2.start();	
+		mp2.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp2)	
+			{	
+				mp2.release();	
+				playActiveWordClip();	
+			}	
+		});	
+	}	
+*/	
+	protected void playCorrectSoundThenActiveWordClip(final boolean playFinalSound)	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		MediaPlayer mp2 = MediaPlayer.create(this, R.raw.zz_correct);	
+		mediaPlayerIsPlaying = true;	
+		mp2.start();	
+		mp2.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp2)	
+			{	
+				mp2.release();	
+				playActiveWordClip(playFinalSound);	
+			}	
+		});	
+	}	
+	protected void playIncorrectSound()	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		MediaPlayer mp3 = MediaPlayer.create(this, R.raw.zz_incorrect);	
+		mediaPlayerIsPlaying = true;	
+		mp3.start();	
+		mp3.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp3)	
+			{	
+				mediaPlayerIsPlaying = false;	
+				setAllTilesClickable();	
+				setOptionsRowClickable();	
+				mp3.release();	
+			}	
+		});	
+	}	
+	protected void playCorrectFinalSound()	
+	{	
+		setAllTilesUnclickable();	
+		setOptionsRowUnclickable();	
+		mediaPlayerIsPlaying = true;	
+		MediaPlayer mp3 = MediaPlayer.create(this, R.raw.zz_correct_final);	
+		mp3.start();	
+		mp3.setOnCompletionListener(new MediaPlayer.OnCompletionListener()	
+		{	
+			@Override	
+			public void onCompletion(MediaPlayer mp3)	
+			{	
+				mediaPlayerIsPlaying = false;	
+				setAllTilesClickable();	
+				setOptionsRowClickable();	
+				mp3.release();	
+			}	
+		});	
+	}	
+	protected void mpCompletion(MediaPlayer mp)	
+	{	
+		mediaPlayerIsPlaying = false;	
+		if (repeatLocked)	
+		{	
+			setAllTilesClickable();	
+		}	
+		setOptionsRowClickable();	
+		mp.release();	
+	}	
+	protected void mpCompletion(MediaPlayer mp, boolean isFinal)	
+	{	
+		if (isFinal)	
+		{	
+			trackerCount++;	
+			updateTrackers();	
+			repeatLocked = false;	
+			SharedPreferences.Editor editor = getSharedPreferences(Start.SHARED_PREFS, MODE_PRIVATE).edit();	
+			String playerString = Util.returnPlayerStringToAppend(playerNumber);	
+			String uniqueGameLevelPlayerID = className + challengeLevel + playerString;	
+			editor.putInt(uniqueGameLevelPlayerID, trackerCount);	
+			editor.apply();	
+			playCorrectFinalSound();	
+		}	
+		else	
+		{	
+			mediaPlayerIsPlaying = false;	
+			if (repeatLocked)	
+			{	
+				setAllTilesClickable();	
+			}	
+			setOptionsRowClickable();	
+			mp.release();	
+		}	
 	}
 
 }
