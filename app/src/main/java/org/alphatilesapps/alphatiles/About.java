32b72e22e5f655a5ac6e6dd9fce70ec0c5f255d4
laineyhm <56163492+laineyhm@users.noreply.github.com>
2021-06-01 21:26:03 -0400

    Update About.java
    Slight change from Keith- added the import of Start

--- app/src/main/java/org/alphatilesapps/alphatiles/About.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/About.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.BuildConfig;
 
+import static org.alphatilesapps.alphatiles.Start.*;
+
 public class About extends AppCompatActivity {
 
     Context context;
