6481dc36f9b300c7d2ca6b6d5d63fa89053464d6
Lainey <laineyhm@gmail.com>
2022-06-24 15:44:38 -0400

    Remove unused imports I had tried out

--- app/src/main/java/org/alphatilesapps/alphatiles/GameActivity.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/GameActivity.java
@@ -28,22 +28,6 @@ import static org.alphatilesapps.alphatiles.Start.incorrectSoundID;
 import static org.alphatilesapps.alphatiles.Start.wordDurations;
 import static org.alphatilesapps.alphatiles.Start.wordAudioIDs;
 
-//LM for forcing RTL layouts
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
 
 public abstract class GameActivity extends AppCompatActivity {
 
