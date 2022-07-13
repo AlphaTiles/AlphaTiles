8d7f6b5a1c048d3933f230752510ace67f1efe02
Jade Phoreman <japhoreman@ucdavis.edu>
2022-07-13 18:59:03 -0500

    beginnings of new Japan game

---
+++ app/src/main/java/org/alphatilesapps/alphatiles/Japan.java
@@ -0,0 +1,14 @@
+package org.alphatilesapps.alphatiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Japan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.japan);
+    }
+}
