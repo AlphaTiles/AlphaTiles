d2a21627df5cba113bc9091c9dc8d4d1a587d098
AaronHemphill <aaron_hemphill@sill.org>
2022-11-30 23:01:00 -0600

    Game of 15 (China.java) now functional, only lacking final sound sequence and advance

--- app/src/main/java/org/alphatilesapps/alphatiles/China.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/China.java
@@ -20,7 +20,7 @@ import androidx.constraintlayout.widget.ConstraintSet;
 public class China extends GameActivity {
     String[][] threeFourWordInLopLwc = new String[3][2];
     String[] oneThreeWordInLopLwc = new String[2];
-    Boolean[] solvedLines = new Boolean[3];
+    Boolean[] solvedLines = new Boolean[4];
     TextView blankTile;
     int moves;
 
@@ -45,7 +45,7 @@ public class China extends GameActivity {
         ImageView instructionsButton = (ImageView) findViewById(R.id.instructions);
         instructionsButton.setVisibility(View.GONE);
 
-        int gameID = R.id.georgiaCL;
+        int gameID = R.id.chinaCL;
         ConstraintLayout constraintLayout = findViewById(gameID);
         ConstraintSet constraintSet = new ConstraintSet();
         constraintSet.clone(constraintLayout);
@@ -77,31 +77,64 @@ public class China extends GameActivity {
         LOGGER.info("Remember: B: post super.onCreate");
         context = this;
         setContentView(R.layout.china);
+        int gameID = R.id.chinaCL;
         LOGGER.info("Remember: C: setContentView complete");
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);     // forces portrait mode only
 
         points = getIntent().getIntExtra("points", 0); // KP
+        chinaPoints = getIntent().getIntExtra("chinaPoints", 0); // LM
+        chinaHasChecked12Trackers = getIntent().getBooleanExtra("chinaHasChecked12Trackers", false);
+
+        String playerString = Util.returnPlayerStringToAppend(playerNumber);
+        SharedPreferences prefs = getSharedPreferences(ChoosePlayer.SHARED_PREFS, MODE_PRIVATE);
+        chinaPoints = prefs.getInt("storedchinaPoints_level" + challengeLevel + "_player"
+                + playerString + "_" + syllableGame, 0);
+        chinaHasChecked12Trackers = prefs.getBoolean("storedchinaHasChecked12Trackers_level"
+                + challengeLevel + "_player" + playerString + "_" + syllableGame, false);
+
         playerNumber = getIntent().getIntExtra("playerNumber", -1); // KP
         challengeLevel = getIntent().getIntExtra("challengeLevel", -1); // KP
 
         LOGGER.info("Remember: D: three intents gotten");
 
-        String gameUniqueID = country.toLowerCase().substring(0,2) + challengeLevel;
-
+        String gameUniqueID = country.toLowerCase().substring(0,2) + challengeLevel + syllableGame;
         setTitle(Start.localAppName + ": " + gameNumber + "    (" + gameUniqueID + ")");
+
+        if (scriptDirection.compareTo("RTL") == 0){ //LM: flips images for RTL layouts. LTR is default
+            ImageView instructionsImage = (ImageView) findViewById(R.id.instructions);
+            ImageView repeatImage = (ImageView) findViewById(R.id.repeatImage);
+            ImageView playNextWordImage = (ImageView) findViewById(R.id.playNextWord);
+            ImageView referenceItemImage = (ImageView) findViewById(R.id.referenceItem);
+
+            instructionsImage.setRotationY(180);
+            repeatImage.setRotationY(180);
+            playNextWordImage.setRotationY(180);
+            referenceItemImage.setRotationY(180);
+
+            fixConstraintsRTL(gameID);
+        }
+
 
         TextView pointsEarned = findViewById(R.id.pointsTextView);
         pointsEarned.setText(String.valueOf(points));
 
-        SharedPreferences prefs = getSharedPreferences(ChoosePlayer.SHARED_PREFS, MODE_PRIVATE);
-        String playerString = Util.returnPlayerStringToAppend(playerNumber);
-        String uniqueGameLevelPlayerID = getClass().getName() + challengeLevel + playerString;
+        String uniqueGameLevelPlayerID = getClass().getName() + challengeLevel + playerString + syllableGame;
         trackerCount = prefs.getInt(uniqueGameLevelPlayerID,0);
 
         updateTrackers();
 
+        if(getAudioInstructionsResID()==0) {
+            centerGamesHomeImage();
+        }
+
+        visibleTiles = 16;
+
         playAgain();
 
+    }
+    @Override
+    public void onBackPressed() {
+        // no action
     }
 
     public void repeatGame(View View) {
@@ -184,6 +217,7 @@ public class China extends GameActivity {
             image.setVisibility(View.VISIBLE);
         }
         tiles.addAll(Start.tileList.parseWordIntoTiles(oneThreeWordInLopLwc[1]));
+        Collections.shuffle(tiles);
 
         ImageView image = findViewById(WORD_IMAGES[3]);
         int resID = getResources().getIdentifier(oneThreeWordInLopLwc[0] + "2", "drawable", getPackageName());
@@ -196,7 +230,6 @@ public class China extends GameActivity {
             setUpTiles();
             return;
         }
-        Collections.shuffle(tiles);
 
         for (int i = 0; i < 15; i++){
             TextView gameTile = findViewById(TILE_BUTTONS[i]);
@@ -211,16 +244,16 @@ public class China extends GameActivity {
         blankTile = finalTile;
 
         Random rand = new Random();
-        int tile;
+        int tileX;
         int lastTile = 16;
 
         while (moves != 0){
-            tile = rand.nextInt(TILE_BUTTONS.length);
+            tileX = rand.nextInt(TILE_BUTTONS.length);
 
-            if (isSlidable(tile) && tile != lastTile) {
-                TextView t = findViewById(TILE_BUTTONS[tile]);
+            if (isSlideable(tileX) && tileX != lastTile) {
+                TextView t = findViewById(TILE_BUTTONS[tileX]);
                 swapTiles(t, blankTile);
-                lastTile = tile;
+                lastTile = tileX;
                 moves--;
             }
         }
@@ -245,10 +278,6 @@ public class China extends GameActivity {
 
     private void respondToTileSelection(int justClickedTile) {
 
-        if (mediaPlayerIsPlaying) {
-            return;
-        }
-
         setAllTilesUnclickable();
         setOptionsRowUnclickable();
 
@@ -262,13 +291,18 @@ public class China extends GameActivity {
 
         LOGGER.info("Remember: tileOfTargetRow = " + tileOfTargetRow);
 
-        int tileNo = justClickedTile - 1; //  justClickedTile uses 1 to 18, t uses the array ID (between [0] and [17]
-        TextView tile = findViewById(TILE_BUTTONS[tileNo]);
+        int tileNo = justClickedTile - 1; //  justClickedTile uses 1 to 16, tileNo uses the array ID (between [0] and [15]
+        TextView tileSelected = findViewById(TILE_BUTTONS[tileNo]);
 
-        if (isSlidable(tileNo)){
-            swapTiles(tile, blankTile);
-            checkLineForSolve(tileOfTargetRow);
+        if (isSlideable(tileNo)){
+            swapTiles(tileSelected, blankTile);
+            LOGGER.info("Remember: pre checkLineForSolve ... tileOfTargetRow = " + tileOfTargetRow);
         }
+
+        checkLineForSolve(1);
+        checkLineForSolve(5);
+        checkLineForSolve(9);
+        checkLineForSolve(13);
 
         if (areAllLinesSolved()) {
             repeatLocked = false;
@@ -288,14 +322,14 @@ public class China extends GameActivity {
             editor.putInt(uniqueGameLevelPlayerID, trackerCount);
             editor.apply();
 
-            for (int tileButton : TILE_BUTTONS) {
-                TextView gameTile = findViewById(tileButton);
-                if (gameTile != blankTile) {
-                    String wordColorStr = "#4CAF50"; // theme green
-                    int wordColorNo = Color.parseColor(wordColorStr);
-                    gameTile.setBackgroundColor(wordColorNo);
-                }
-            }
+//            for (int tileButton : TILE_BUTTONS) {
+//                TextView gameTile = findViewById(tileButton);
+//                if (gameTile != blankTile) {
+//                    String wordColorStr = "#4CAF50"; // theme green
+//                    int wordColorNo = Color.parseColor(wordColorStr);
+//                    gameTile.setBackgroundColor(wordColorNo);
+//                }
+//            }
             setOptionsRowClickable();
         }
         else {
@@ -309,12 +343,12 @@ public class China extends GameActivity {
         respondToTileSelection(Integer.parseInt((String)view.getTag())); // KP
     }
 
-    private void checkLineForSolve(int tile){
+    private void checkLineForSolve(int tileInRowToCheck){
 
-        LOGGER.info("Remember: just started private void checkLineForSolve(int tile) ");
-        LOGGER.info("Remember: checkLineForSolve tile = " + tile);
+        LOGGER.info("Remember: just started private void checkLineForSolve(int tileInRowToCheck) ");
+        LOGGER.info("Remember: checkLineForSolve tileInRowToCheck = " + tileInRowToCheck);
 
-        int row = ((tile - 1) / 4) + 1;
+        int row = ((tileInRowToCheck - 1) / 4) + 1;
         LOGGER.info("Remember: row = " + row);
         int leftMostTile = (row - 1) * 4;
         LOGGER.info("Remember: leftMostTile = " + leftMostTile);
@@ -325,7 +359,7 @@ public class China extends GameActivity {
             correctWord = Start.wordList.stripInstructionCharacters(threeFourWordInLopLwc[row - 1][1]);
             LOGGER.info("Remember: correctWord = " + correctWord);
         } else {
-            correctWord = Start.wordList.stripInstructionCharacters(oneThreeWordInLopLwc[0]);
+            correctWord = Start.wordList.stripInstructionCharacters(oneThreeWordInLopLwc[1]);
             LOGGER.info("Remember: correctWord = " + correctWord);
         }
         TextView gameTile1 = findViewById(TILE_BUTTONS[leftMostTile]);
@@ -333,29 +367,52 @@ public class China extends GameActivity {
         TextView gameTile3 = findViewById(TILE_BUTTONS[leftMostTile + 2]);
         TextView gameTile4 = findViewById(TILE_BUTTONS[leftMostTile + 3]);
         gridWord = gameTile1.getText().toString() + gameTile2.getText().toString() + gameTile3.getText().toString() + gameTile4.getText().toString();
-//        gridWord = gridWord.trim(); // remove space in slot 16 - NO SPACE, JUST "", SO NOT NEEDED
+
+        if(row == 4) {
+            if (blankTile.getTag().equals("14") || blankTile.getTag().equals("15")) {
+                gridWord = ""; // For the word "cat", will only accept |c|a|t| | or | |c|a|t| but not |c| |a|t| or |c|a| |t|
+            }
+        }
+
         LOGGER.info("Remember: gridWord = " + gridWord);
         if (gridWord.equals(correctWord)) {
-            solvedLines[row - 1] = true;
             LOGGER.info("Remember: gridWord matches correctWord");
+            solvedLines[row - 1] = true;
+            LOGGER.info("Remember: solvedLines set to true");
             for (int i = leftMostTile; i <= (leftMostTile + 3); i++) {
                 LOGGER.info("Remember: i = " + i);
                 TextView gameTile = findViewById(TILE_BUTTONS[i]);
-                String wordColorStr = "#4CAF50"; //theme green
-                int wordColorNo = Color.parseColor(wordColorStr);
-                gameTile.setBackgroundColor(wordColorNo);
-                LOGGER.info("Remember: background color changed");
+                if (gameTile == blankTile) {
+                    String wordColorStr = "#FFFFFF"; //white
+                    int wordColorNo = Color.parseColor(wordColorStr);
+                    gameTile.setBackgroundColor(wordColorNo);
+                    LOGGER.info("Remember: background color changed");
+                } else {
+                    String wordColorStr = "#4CAF50"; //theme green
+                    int wordColorNo = Color.parseColor(wordColorStr);
+                    gameTile.setBackgroundColor(wordColorNo);
+                    LOGGER.info("Remember: background color changed");
+                }
             }
         } else {
-            solvedLines[row - 1] = false;
             LOGGER.info("Remember: gridWord does NOT match correctWord");
+            LOGGER.info("Remember: row = " + row);
+            solvedLines[row - 1] = false;
+            LOGGER.info("Remember: solvedLines set to false");
             for (int i = leftMostTile; i <= (leftMostTile + 3); i++) {
                 LOGGER.info("Remember: i = " + i);
                 TextView gameTile = findViewById(TILE_BUTTONS[i]);
-                String wordColorStr = "#000000"; //black
-                int wordColorNo = Color.parseColor(wordColorStr);
-                gameTile.setBackgroundColor(wordColorNo);
-                LOGGER.info("Remember: background color changed");
+                if (gameTile == blankTile) {
+                    String wordColorStr = "#FFFFFF"; //white
+                    int wordColorNo = Color.parseColor(wordColorStr);
+                    gameTile.setBackgroundColor(wordColorNo);
+                    LOGGER.info("Remember: background color changed");
+                } else {
+                    String wordColorStr = "#000000"; //black
+                    int wordColorNo = Color.parseColor(wordColorStr);
+                    gameTile.setBackgroundColor(wordColorNo);
+                    LOGGER.info("Remember: background color changed");
+                }
             }
         }
     }
@@ -371,31 +428,32 @@ public class China extends GameActivity {
 
     }
 
-    private boolean isSlidable(int tileNo){
-        boolean Slidable = false;
-        TextView tile;
+    private boolean isSlideable(int tileNo){
+        boolean slideable = false;
+        TextView tileToCheck;
 
         if (tileNo != 0 && tileNo != 4 && tileNo != 8 && tileNo != 12){
-            tile = findViewById(TILE_BUTTONS[tileNo - 1]);
-            Slidable = (tile == blankTile);
+            tileToCheck = findViewById(TILE_BUTTONS[tileNo - 1]);
+            slideable = (tileToCheck == blankTile);
         }
 
-        if (tileNo != 3 && tileNo != 7 && tileNo != 11 && tileNo != 15 && !Slidable){
-            tile = findViewById(TILE_BUTTONS[tileNo + 1]);
-            Slidable = (tile == blankTile);
+        if (tileNo != 3 && tileNo != 7 && tileNo != 11 && tileNo != 15 && !slideable){
+            tileToCheck = findViewById(TILE_BUTTONS[tileNo + 1]);
+            slideable = (tileToCheck == blankTile);
         }
 
-        if (tileNo >= 4 && !Slidable){
-            tile = findViewById(TILE_BUTTONS[tileNo - 4]);
-            Slidable = (tile == blankTile);
+        if (tileNo >= 4 && !slideable){
+            tileToCheck = findViewById(TILE_BUTTONS[tileNo - 4]);
+            slideable = (tileToCheck == blankTile);
         }
 
-        if (tileNo < 12 && !Slidable){
-            tile = findViewById(TILE_BUTTONS[tileNo + 4]);
-            Slidable = (tile == blankTile);
+        if (tileNo < 12 && !slideable){
+            tileToCheck = findViewById(TILE_BUTTONS[tileNo + 4]);
+            slideable = (tileToCheck == blankTile);
         }
 
-        return Slidable;
+        LOGGER.info("Remember: slideable " + slideable);
+        return slideable;
     }
 
     @Override
