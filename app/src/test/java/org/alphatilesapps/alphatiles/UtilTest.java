c946f7388db159a887e1a626d62ac94e63cc639d
Darcy Wong <darcy_wong@sil.org>
2023-03-11 17:31:52 +0700

    Add UtilTest and robolectric

---
+++ app/src/test/java/org/alphatilesapps/alphatiles/UtilTest.java
@@ -0,0 +1,23 @@
+package org.alphatilesapps.alphatiles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class UtilTest {
+
+    @Test
+    public void test_returnPlayerStringToAppend() {
+
+        // This is probably invalid. Should expect "-01" or throw?
+        assertEquals("0-1", Util.returnPlayerStringToAppend(-1));
+
+        assertEquals("00", Util.returnPlayerStringToAppend(0));
+        assertEquals("09", Util.returnPlayerStringToAppend(9));
+        assertEquals("10", Util.returnPlayerStringToAppend(10));
+    }
+}
