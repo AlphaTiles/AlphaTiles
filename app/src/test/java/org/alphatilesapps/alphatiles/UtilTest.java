c4c5fc56b016adca6f507c478fe27926c686e40a
Fiona Kriner <jkriner13@gmail.com>
2024-07-07 14:59:39 -0500

    move some code for clarity

--- app/src/test/java/org/alphatilesapps/alphatiles/UtilTest.java
+++ app/src/test/java/org/alphatilesapps/alphatiles/UtilTest.java
@@ -25,4 +25,7 @@ public class UtilTest {
     public void play_tile_audio_doesnt_crash_if_tile_has_no_audio() {
         // insert here
     }
+
+    @Test
+    public void play_tile_audio_doesnt_
 }
