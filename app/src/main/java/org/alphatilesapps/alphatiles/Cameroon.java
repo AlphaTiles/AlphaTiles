1a76a6092848086aad40a433c0de364d7f048d2b
AaronHemphill <alphatilesapps@gmail.com>
2026-07-21 14:45:52 -0500

    Rename and reorder methods to more closely follow other games; update CL definitions so easier level is 1 in all three cases

--- app/src/main/java/org/alphatilesapps/alphatiles/Cameroon.java
+++ app/src/main/java/org/alphatilesapps/alphatiles/Cameroon.java
@@ -35,14 +35,43 @@ public class Cameroon extends GameActivity {
     int[] selectors = {R.id.selector0, R.id.selector1, R.id.selector2, R.id.selector3};
     final int selectorColor = Color.parseColor(colorList.get(5));
     final int selectorColorCorrect = Color.parseColor(colorList.get(3));
+
+    @Override
+    protected int[] getGameButtons() {
+        return new int[]{R.id.selector0, R.id.selector1, R.id.selector2, R.id.selector3};
+    }
+
+    @Override
+    protected int[] getWordImages() {
+        return null;
+    }
+
+    @Override
+    protected int getAudioInstructionsResID() {
+        Resources res = context.getResources();
+        int audioInstructionsResID;
+        try {
+            audioInstructionsResID = res.getIdentifier(Start.gameList.get(gameNumber - 1).instructionAudioName, "raw", context.getPackageName());
+        } catch (NullPointerException e) {
+            audioInstructionsResID = -1;
+        }
+        return audioInstructionsResID;
+    }
+
+    @Override
+    protected void hideInstructionAudioImage() {
+        ImageView instructionsButton = findViewById(R.id.instructions);
+        instructionsButton.setVisibility(View.GONE);
+    }
+
     @Override
     protected void onCreate(Bundle state) {
         super.onCreate(state);
         context = this;
         // challengeLevel has three slots, each with a 1 or 2, i.e. 111 = use image + substitute one random slot + substitute with
-        useImage = challengeLevel / 100 != 2; // slot 1: TRUE = image visible, FALSE = image hidden
-        substituteAll = (challengeLevel / 10) % 10 == 2; // slot 2: TRUE = substitute all, FALSE = substitute one
-        useDistractors = challengeLevel % 10 == 2; // slot 3: TRUE = substitute with distractors, FALSE = substitute with random
+        useImage = challengeLevel / 100 != 2; // slot 1: 1__ --> TRUE = image visible, 2__ --> FALSE = image hidden
+        substituteAll = (challengeLevel / 10) % 10 != 2; // slot 2: _1_ --> TRUE = substitute all, _2_ --> FALSE = substitute one
+        useDistractors = challengeLevel % 10 == 2; // slot 3: __2 --> TRUE = substitute with distractors, __1 --> FALSE = substitute with random
         setContentView(R.layout.cameroon);
         ActivityLayouts.applyEdgeToEdge(this, R.id.cameroonCL);
         ActivityLayouts.setStatusAndNavColors(this);
@@ -72,7 +101,7 @@ public class Cameroon extends GameActivity {
         for (int idx = 0; idx < 4; idx++) {
             View selector = findViewById(selectors[idx]);
             int i = idx;
-            selector.setOnClickListener((view) -> onSelector(selector, i));
+            selector.setOnClickListener((view) -> respondToSelectorSelection(selector, i));
             GradientDrawable circle = new GradientDrawable();
             circle.setShape(GradientDrawable.OVAL);
             circle.setColor(color);
@@ -82,93 +111,60 @@ public class Cameroon extends GameActivity {
 
     }
 
-    private void onSelector(View selector, int idx) {
-        if(!repeatLocked) return;
-        if (idx == correct) {
-            GradientDrawable circle = (GradientDrawable) selector.getBackground();
-            circle.setColor(selectorColorCorrect);
-            repeatLocked = false;
-            setAdvanceArrowToBlue();
-            updatePointsAndTrackers(1);
-            if (sendAnalytics) {
-                String gameUniqueID = country.toLowerCase().substring(0, 2) + challengeLevel + syllableGame;
-                Properties info = new Properties().putValue("Time Taken", System.currentTimeMillis() - levelBegunTime)
-                        .putValue("Number Incorrect", incorrectOnLevel)
-                        .putValue("Correct Answer", correctString)
-                        .putValue("Grade", studentGrade);
-                for (int i = 0; i < 4; i++) {
-                    if (!incorrectAnswersSelected.get(i).isEmpty()) {
-                        info.putValue("Incorrect_" + (i + 1), incorrectAnswersSelected.get(i));
-                    }
-                }
-                Analytics.with(context).track(gameUniqueID, info);
-            }
-            playCorrectSoundThenActiveWordClip(false);
-        } else {
-            // TODO: this won't work for Thai, but the method for combining Thai characters only works when only one tile is changed
-            StringBuilder incorrect = new StringBuilder();
-            for (Start.Tile tile : words.get(idx)) {
-                incorrect.append(tile.text);
-            }
-            incorrectAnswersSelected.set(idx, incorrect.toString());
+    public void repeatGame(View View) {
+        if (!repeatLocked && !mediaPlayerIsPlaying) {
+            playAgain();
         }
     }
 
-    private List<Start.Tile> tilesOfType(String type) {
-        switch (type) {
-            case "V":
-                return Start.VOWELS;
-            case "C":
-                return Start.CONSONANTS;
-            case "T":
-                return Start.TONES;
-            default:
-                return null;
+    public void playAgain() {
+        for (int idx = 0; idx < 4; idx++) {
+            View selector = findViewById(selectors[idx]);
+            GradientDrawable circle = new GradientDrawable();
+            circle.setShape(GradientDrawable.OVAL);
+            circle.setColor(selectorColor);
+            selector.setBackground(circle);
         }
-    }
-
-    private Start.Tile findTile(String text, String type) {
-        Start.Tile fallback = null;
-        for (Start.Tile tile : Start.tileList) {
-            if (fallback == null && tile.text.equals(text))
-                fallback = tile;
-            if (tile.text.equals(text) && tile.typeOfThisTileInstance.equals(type))
-                return tile;
+        incorrectAnswersSelected = new ArrayList<>(4);
+        for (int i = 0; i < 4; i++) {
+            incorrectAnswersSelected.add("");
         }
-        return fallback;
-    }
-
-    private <T> List<T> choose(List<T> list, int n) {
-        if (n > list.size()) return null;
-        Random rand = new Random();
-        List<Integer> indices = new ArrayList<>();
-        List<T> out = new ArrayList<>();
-        while (indices.size() < n) {
-            while (true) {
-                int x = rand.nextInt(list.size());
-                if (indices.contains(x)) continue;
-                indices.add(x);
-                out.add(list.get(x));
-                break;
+        repeatLocked = true;
+        setAdvanceArrowToGray();
+        do {
+            chooseWord();
+            parsedRefWordTileArray = Start.tileList.parseWordIntoTiles(refWord.wordInLOP, refWord);
+        } while (parsedRefWordTileArray.size() > 7);
+        if (useImage) {
+            int resID = getResources().getIdentifier(refWord.wordInLWC, "drawable", getPackageName());
+            ImageView wordImage = findViewById(R.id.wordImage);
+            wordImage.setImageResource(resID);
+        }
+        TextView text = findViewById(R.id.wordText);
+        text.setText(wordList.stripInstructionCharacters(refWord.wordInLOP));
+        correct = new Random().nextInt(4);
+        words = mutateWord(parsedRefWordTileArray);
+        words.add(correct, parsedRefWordTileArray);
+        correctString = Start.wordList.stripInstructionCharacters(refWord.wordInLOP);
+        for (int row = 0; row < 4; row++) {
+            LinearLayout layout = rows.get(row);
+            layout.removeAllViews();
+            List<Start.Tile> word = words.get(row);
+            for (int col = 0; col < word.size(); col++) {
+                layout.addView(tileButton(word.get(col)));
             }
         }
-        return out;
-    }
 
-    private List<Start.Tile> replacementTiles(Start.Tile tile, boolean useDistractors) {
-        List<Start.Tile> tiles;
-        String type = tile.typeOfThisTileInstance;
-        if (useDistractors) {
-            tiles = new ArrayList<>();
-            // This is a little weird because we throw it out half the time, but it makes it simpler :sob:
-            tiles.add(tile);
-            for (String distractor : tile.distractors) {
-                tiles.add(findTile(distractor, type));
-            }
-        } else {
-            tiles = tilesOfType(type);
+        for (int i = 0; i < 3; i++) {
+            incorrectAnswersSelected.set(i, "");
         }
-        return tiles;
+        incorrectOnLevel = 0;
+        levelBegunTime = System.currentTimeMillis();
+
+        playActiveWordClip(false);
+        setAllGameButtonsClickable();
+        setOptionsRowClickable();
+
     }
 
     private List<List<Start.Tile>> mutateWord(ArrayList<Start.Tile> word) {
@@ -214,8 +210,13 @@ public class Cameroon extends GameActivity {
                 taken.add(new ArrayList<>());
             }
             for(int row = 0; row < 3; row++) {
-                int col = rand.nextInt(word.size());
-                List<Start.Tile> replacements = replacementTiles(word.get(col), useDistractors);
+                int col;
+                List<Start.Tile> replacements;
+                do {
+                    col = rand.nextInt(word.size());
+                    replacements = replacementTiles(word.get(col), useDistractors);
+                }
+                while (replacements == null);
                 Start.Tile tile;
                 while(true) {
                     tile = replacements.get(rand.nextInt(replacements.size()));
@@ -229,48 +230,61 @@ public class Cameroon extends GameActivity {
         return rows;
     }
 
-    public void playAgain() {
-        for (int idx = 0; idx < 4; idx++) {
-            View selector = findViewById(selectors[idx]);
-            GradientDrawable circle = new GradientDrawable();
-            circle.setShape(GradientDrawable.OVAL);
-            circle.setColor(selectorColor);
-            selector.setBackground(circle);
+    private List<Start.Tile> replacementTiles(Start.Tile tile, boolean useDistractors) {
+        List<Start.Tile> tiles;
+        String type = tile.typeOfThisTileInstance;
+        if (useDistractors) {
+            tiles = new ArrayList<>();
+            // This is a little weird because we throw it out half the time, but it makes it simpler :sob:
+            tiles.add(tile);
+            for (String distractor : tile.distractors) {
+                tiles.add(findTile(distractor, type));
+            }
+        } else {
+            tiles = tilesOfType(type);
         }
-        incorrectAnswersSelected = new ArrayList<>(4);
-        for (int i = 0; i < 4; i++) {
-            incorrectAnswersSelected.add("");
-        }
-        repeatLocked = true;
-        setAdvanceArrowToGray();
-        do {
-            chooseWord();
-            parsedRefWordTileArray = Start.tileList.parseWordIntoTiles(refWord.wordInLOP, refWord);
-        } while (parsedRefWordTileArray.size() > 7);
-        if (useImage) {
-            int resID = getResources().getIdentifier(refWord.wordInLWC, "drawable", getPackageName());
-            ImageView wordImage = findViewById(R.id.wordImage);
-            wordImage.setImageResource(resID);
-        }
-        TextView text = findViewById(R.id.wordText);
-        text.setText(wordList.stripInstructionCharacters(refWord.wordInLOP));
-        correct = new Random().nextInt(4);
-        words = mutateWord(parsedRefWordTileArray);
-        words.add(correct, parsedRefWordTileArray);
-        correctString = Start.wordList.stripInstructionCharacters(refWord.wordInLOP);
-        for (int row = 0; row < 4; row++) {
-            LinearLayout layout = rows.get(row);
-            layout.removeAllViews();
-            List<Start.Tile> word = words.get(row);
-            for (int col = 0; col < word.size(); col++) {
-                layout.addView(tileButton(word.get(col)));
+        return tiles;
+    }
+
+    private <T> List<T> choose(List<T> list, int n) {
+        if (n > list.size()) return null;
+        Random rand = new Random();
+        List<Integer> indices = new ArrayList<>();
+        List<T> out = new ArrayList<>();
+        while (indices.size() < n) {
+            while (true) {
+                int x = rand.nextInt(list.size());
+                if (indices.contains(x)) continue;
+                indices.add(x);
+                out.add(list.get(x));
+                break;
             }
         }
+        return out;
+    }
 
-        playActiveWordClip(false);
-        setAllGameButtonsClickable();
-        setOptionsRowClickable();
+    private Start.Tile findTile(String text, String type) {
+        Start.Tile fallback = null;
+        for (Start.Tile tile : Start.tileList) {
+            if (fallback == null && tile.text.equals(text))
+                fallback = tile;
+            if (tile.text.equals(text) && tile.typeOfThisTileInstance.equals(type))
+                return tile;
+        }
+        return fallback;
+    }
 
+    private List<Start.Tile> tilesOfType(String type) {
+        switch (type) {
+            case "V":
+                return Start.VOWELS;
+            case "C":
+                return Start.CONSONANTS;
+            case "T":
+                return Start.TONES;
+            default:
+                return null;
+        }
     }
 
     private ImageView tileButton(Start.Tile tile) {
@@ -300,9 +314,40 @@ public class Cameroon extends GameActivity {
         return view;
     }
 
-    public void repeatGame(View View) {
-        if (!repeatLocked && !mediaPlayerIsPlaying) {
-            playAgain();
+    private void respondToSelectorSelection(View selector, int idx) {
+        if(!repeatLocked) return;
+        if (idx == correct) {
+            GradientDrawable circle = (GradientDrawable) selector.getBackground();
+            circle.setColor(selectorColorCorrect);
+            repeatLocked = false;
+            setAdvanceArrowToBlue();
+            updatePointsAndTrackers(1);
+            if (sendAnalytics) {
+                String gameUniqueID = country.toLowerCase().substring(0, 2) + challengeLevel + syllableGame;
+                Properties info = new Properties().putValue("Time Taken", System.currentTimeMillis() - levelBegunTime)
+                        .putValue("Number Incorrect", incorrectOnLevel)
+                        .putValue("Correct Answer", correctString)
+                        .putValue("Grade", studentGrade);
+                for (int i = 0; i < 4; i++) {
+                    if (!incorrectAnswersSelected.get(i).isEmpty()) {
+                        info.putValue("Incorrect_" + (i + 1), incorrectAnswersSelected.get(i));
+                    }
+                }
+                Analytics.with(context).track(gameUniqueID, info);
+            }
+            playCorrectSoundThenActiveWordClip(false);
+            for (int s : selectors) {
+                findViewById(s).setClickable(false); // after the round ends, a player can still listen to tile audio and review why they were right or wrong
+            }
+        } else {
+            // TODO: this won't work for Thai, but the method for combining Thai characters only works when only one tile is changed
+            incorrectOnLevel += 1;
+            StringBuilder incorrect = new StringBuilder();
+            for (Start.Tile tile : words.get(idx)) {
+                incorrect.append(tile.text);
+            }
+            incorrectAnswersSelected.set(idx, incorrect.toString());
+            playIncorrectSound();
         }
     }
 
@@ -310,31 +355,14 @@ public class Cameroon extends GameActivity {
         super.clickPicHearAudio(view);
     }
 
-    @Override
-    protected int[] getGameButtons() {
-        return new int[]{R.id.selector0, R.id.selector1, R.id.selector2, R.id.selector3};
+    public void goBackToEarth(View view) {
+        super.goBackToEarth(view);
     }
 
-    @Override
-    protected int[] getWordImages() {
-        return null;
-    }
-
-    @Override
-    protected int getAudioInstructionsResID() {
-        Resources res = context.getResources();
-        int audioInstructionsResID;
-        try {
-            audioInstructionsResID = res.getIdentifier(Start.gameList.get(gameNumber - 1).instructionAudioName, "raw", context.getPackageName());
-        } catch (NullPointerException e) {
-            audioInstructionsResID = -1;
+    public void playAudioInstructions(View view) {
+        if (getAudioInstructionsResID() > 0) {
+            super.playAudioInstructions(view);
         }
-        return audioInstructionsResID;
     }
 
-    @Override
-    protected void hideInstructionAudioImage() {
-        ImageView instructionsButton = findViewById(R.id.instructions);
-        instructionsButton.setVisibility(View.GONE);
-    }
 }
